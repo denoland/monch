f0093a25af4f6ee68da514cf83f44ef4d1d72d34
David Sherret <dsherret@users.noreply.github.com>
2023-05-06 20:05:04 -0400

    feat: ability to create ParseErrorFailureError (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,12 @@ impl<'a> ParseErrorFailure<'a> {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct ParseErrorFailureError(String);
 
+impl ParseErrorFailureError {
+  pub fn new(message: String) -> Self {
+    ParseErrorFailureError(message)
+  }
+}
+
 impl std::fmt::Display for ParseErrorFailureError {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "{}", self.0)
